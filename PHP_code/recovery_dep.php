c42528a6d8774173448c5258858cd8f0f1bb0a31
Alexander Nagel <t00189485@365s.ittralee.ie>
2018-04-13 16:48:45 +0100

    4 states are added

--- PHP_code/recovery_dep.php
+++ PHP_code/recovery_dep.php
@@ -14,6 +14,7 @@ require_once 'config.php';
 $pname = $ptype = $bayno = "";
 $pname_err = $ptype_err = $bayno_err = "";
 $patient_status = "Not yet available";
+$var = microtime(true);
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -127,6 +128,142 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   height : 100%;
   color : #3d23;
 }
+/* visited link */
+a:visited {
+    color: #ede7e3;
+}
+
+/* mouse over link */
+a:hover {
+    color: rgba(2, 12, 3, 0.78);
+}
+
+/* selected link */
+a:active {
+    color: #ede7e3;
+}
+.button-container {
+    width: auto;
+    overflow-y: auto;
+}
+
+.button-container > a {
+    float: left;
+    text-decoration: none;
+    border-radius: 10px;
+    margin: 10px;
+}
+.button{
+    z-index: 1;
+    width: 10em;
+    height: 10em;
+    padding-top: 4em;
+    background: rgba(32, 135, 41, 0.60);
+    font-weight:  bold;
+}
+.buttonAssigned{
+    z-index: 1;
+    width: 10em;
+    height: 10em;
+    padding-top: 4em;
+    background: rgba(59, 150, 216, 0.85);
+    font-weight:  bold;
+}
+.buttonAccepted{
+    z-index: 1;
+    width: 10em;
+    height: 10em;
+    padding-top: 4em;
+    background: rgba(205, 16, 222, 0.85);
+    font-weight:  bold;
+}
+.buttonAlarm {
+  z-index: 1;
+  background-color: #FFFFFF;
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+  border: none;
+  color: #FFFFFF;
+  width: 10em;
+  height: 10em;
+  padding-top: 2em;
+  font-weight:  bold;
+  cursor: pointer;
+  display: inline-block;
+  text-align: center;
+  text-decoration: none;
+  -webkit-animation: glowing 1000ms infinite;
+  -moz-animation: glowing 1000ms infinite;
+  -o-animation: glowing 1000ms infinite;
+  animation: glowing 1000ms infinite;
+}
+@-webkit-keyframes glowing {
+  0% { background-color: rgba(208, 22, 5, 0.72); -webkit-box-shadow: 0 0 1px rgba(208, 22, 5, 0.72); }
+  50% { background-color: rgba(231, 3, 7, 0.78); -webkit-box-shadow: 0 0 5px rgba(231, 3, 7, 0.78); }
+  100% { background-color: rgba(208, 22, 5, 0.72); -webkit-box-shadow: 0 0 1px rgba(208, 22, 5, 0.72); }
+}
+
+@-moz-keyframes glowing {
+  0% { background-color: rgba(208, 22, 5, 0.72); -moz-box-shadow: 0 0 1px rgba(208, 22, 5, 0.72); }
+  50% { background-color: rgba(231, 3, 7, 0.78); -moz-box-shadow: 0 0 5px rgba(231, 3, 7, 0.78); }
+  100% { background-color: rgba(208, 22, 5, 0.72); -moz-box-shadow: 0 0 1px rgba(208, 22, 5, 0.72); }
+}
+
+@-o-keyframes glowing {
+  0% { background-color: rgba(208, 22, 5, 0.72); box-shadow: 0 0 1px rgba(208, 22, 5, 0.72); }
+  50% { background-color: rgba(231, 3, 7, 0.78); box-shadow: 0 0 5px rgba(231, 3, 7, 0.78); }
+  100% { background-color: rgba(208, 22, 5, 0.72); box-shadow: 0 0 1px rgba(208, 22, 5, 0.72); }
+}
+
+@keyframes glowing {
+  0% { background-color: rgba(208, 22, 5, 0.72); box-shadow: 0 0 1px rgba(208, 22, 5, 0.72); }
+  50% { background-color: rgba(231, 3, 7, 0.78); box-shadow: 0 0 5px rgba(231, 3, 7, 0.78); }
+  100% { background-color: rgba(208, 22, 5, 0.72); box-shadow: 0 0 1px rgba(208, 22, 5, 0.72); }
+}
+.buttonPending {
+  z-index: 1;
+  background-color: #FFFFFF;
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+  border: none;
+  color: #FFFFFF;
+  width: 10em;
+  height: 10em;
+  padding-top: 4em;
+  font-weight:  bold;
+  cursor: pointer;
+  display: inline-block;
+  text-align: center;
+  text-decoration: none;
+  -webkit-animation: glowing1 1500ms infinite;
+  -moz-animation: glowing1 1500ms infinite;
+  -o-animation: glowing1 1500ms infinite;
+  animation: glowing1 1500ms infinite;
+}
+@-webkit-keyframes glowing1 {
+  0% { background-color: rgba(157, 74, 15, 0.79); -webkit-box-shadow: 0 0 1px rgba(157, 74, 15, 0.79);}
+  50% { background-color: rgba(204, 85, 0, 0.85); -webkit-box-shadow: 0 0 5px rgba(204, 85, 0, 0.85); }
+  100% { background-color: rgba(229, 95, 0, 0.88); -webkit-box-shadow: 0 0 1px rgba(229, 95, 0, 0.88); }
+}
+
+@-moz-keyframes glowing1 {
+  0% { background-color: rgba(157, 74, 15, 0.79); -moz-box-shadow: 0 0 1px rgba(157, 74, 15, 0.79); }
+  50% { background-color: rgba(204, 85, 0, 0.85); -moz-box-shadow: 0 0 5px rgba(204, 85, 0, 0.85); }
+  100% { background-color: rgba(229, 95, 0, 0.88); -moz-box-shadow: 0 0 1px rgba(229, 95, 0, 0.88); }
+}
+
+@-o-keyframes glowing1 {
+  0% { background-color: rgba(157, 74, 15, 0.79); box-shadow: 0 0 1px rgba(157, 74, 15, 0.79); }
+  50% { background-color: rgba(204, 85, 0, 0.85); box-shadow: 0 0 5px rgba(204, 85, 0, 0.85); }
+  100% { background-color: rgba(229, 95, 0, 0.88); box-shadow: 0 0 1px rgba(229, 95, 0, 0.88); }
+}
+
+@keyframes glowing1 {
+  0% { background-color: rgba(157, 74, 15, 0.79); box-shadow: 0 0 1px rgba(157, 74, 15, 0.79); }
+  50% { background-color: rgba(204, 85, 0, 0.85); box-shadow: 0 0 5px rgba(204, 85, 0, 0.85); }
+  100% { background-color: rgba(229, 95, 0, 0.88); box-shadow: 0 0 1px rgba(229, 95, 0, 0.88); }
+}
+
 @media (max-width: 1200px) {
   .left{
     height:50%
@@ -146,6 +283,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </style>
 </head>
 <body>
+  <script type="text/javascript">
+  function alarmFunction() {
+    element = document.getElementById("bay1Button");
+    element.className = element.className.replace(/\bbutton\b/g, "buttonAlarm");
+    element.innerHTML = 'Bay 1 <br> Abnormal delay <br> Request has been sent';
+  }
+  function pendingFunction() {
+    element = document.getElementById("bay2Button");
+    element.className = element.className.replace(/\bbutton\b/g, "buttonPending");
+    element.innerHTML = 'Bay 2 <br> Pending transfer';
+  }
+  function assignFunction() {
+    element = document.getElementById("bay3Button");
+    element.className = element.className.replace(/\bbutton\b/g, "buttonAssigned");
+    element.innerHTML = 'Bay 3 <br> Patient assigned';
+  }
+  function acceptedFunction() {
+    element = document.getElementById("bay4Button");
+    element.className = element.className.replace(/\bbutton\b/g, "buttonAccepted");
+    element.innerHTML = 'Bay 3 <br> Transfer accepted';
+  }
+  </script>
+
   <div class="page-header">
       <h4>RECOVERY WARD DEPARTMENT.</h4>
   </div>
@@ -183,9 +343,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </div>
       </div>
       <div class="col-lg-6 col-sm-12 right">
-        <p>
-          right pannel
-        </p>
+        <div class="button-container">
+          <a id=bay1Button href="#/Action1" class="button" onclick="alarmFunction();">Bay 1 <br> Ready</a>
+          <a id=bay2Button href="#/Action2" class="button" onclick="pendingFunction();">Bay 2 <br> Ready</a>
+          <a id=bay3Button href="#/Action3" class="button" onclick="assignFunction();">Bay 3 <br> Ready</a>
+          <a id=bay4Button href="#/Action4" class="button" onclick="acceptedFunction();">Bay 4 <br> Ready</a>
+          <a href="#/Action5" class="button">Bay 5 <br> Ready</a>
+          <a href="#/Action6" class="button">Bay 6 <br> Ready</a>
+          <a href="#/Action4" class="button">Bay 7 <br> Ready</a>
+          <a href="#/Action5" class="button">Bay 8 <br> Ready</a>
+        </div>
       </div>
     </row>
   </section>
@@ -197,5 +364,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </div>
   </section>
   <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
+  <?php echo $var ?>
+
+
 </body>
 </html>
