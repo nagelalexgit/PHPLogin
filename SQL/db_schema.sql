0ae291775ee460d0b31fde25c7148f89f1ff000a
Alexander Nagel <t00189485@365s.ittralee.ie>
2018-04-16 12:12:14 +0100

    modify db

--- SQL/db_schema.sql
+++ SQL/db_schema.sql
@@ -4,6 +4,7 @@ CREATE SCHEMA IF NOT EXISTS `testDB` ;
 USE testDB;
 
 DROP TABLE IF EXISTS `emps`;
+DROP TABLE IF EXISTS `patients`;
 DROP TABLE IF EXISTS `bayregister`;
 
 CREATE TABLE emps (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -12,13 +13,18 @@ CREATE TABLE emps (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 						department VARCHAR(50), 
 							reg_date DATETIME DEFAULT CURRENT_TIMESTAMP);
                             
-CREATE TABLE bayregister (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE patients (pid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 				pname VARCHAR(50) NOT NULL, 
 					ptype VARCHAR(25) NOT NULL,
-						bayno int, 
 							reg_date DATETIME DEFAULT CURRENT_TIMESTAMP);
 
+                            
+CREATE TABLE bayregister (id INT NOT NULL PRIMARY KEY,
+				pid int NOT NULL, assignStatus bool, assignTime timestamp,
+					reqStatus bool, reqTime timestamp, transStatus bool, transTime timestamp,
+						alarmStatus bool, alarmTime timestamp, 
+							note VARCHAR(255));
+                            
 
-select * from emps;
-select * from bayregister;
+ 
 
